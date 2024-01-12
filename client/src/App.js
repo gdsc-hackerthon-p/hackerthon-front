7c84ef01f2ba155007dd5fd81c5e241882c1edba
chan young <white1614@naver.com>
2024-01-12 15:45:08 +0900

    메인 페이지 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
-
-
+import { Route, Routes } from 'react-router-dom';
+import Main from './pages/Main';
 
 function App() {
   return (
     <div className="App">
-
+      <Routes>
+        <Route path='/' element={<Main/>}/>
+      </Routes>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -3,11 +3,18 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { BrowserRouter } from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+  <QueryClientProvider client={queryClient}>
+    <App /> 
+  </QueryClientProvider>
+  </BrowserRouter>
 );
 
 reportWebVitals();
